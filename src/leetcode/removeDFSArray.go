ce187d283711803d4e84893b36d9d822b4953cc5
Albert Liu <540938283@qq.com>
2019-12-12 15:41:18 +0800

    leetcode 2019.12.12

--- src/leetcode/removeDFSArray.go
+++ src/leetcode/removeDFSArray.go
@@ -1,14 +1,10 @@
 package leetcode
 
 func RemoveDuplicates(nums []int) int {
-
-	if len(nums) < 1 {
-		return 1
-	}
 	// 设置
 	p := 0
 
-	for i:=1;i<len(nums);i++ {
+	for i := 1; i < len(nums); i++ {
 		if nums[p] != nums[i] {
 			p++
 			nums[p] = nums[i]
